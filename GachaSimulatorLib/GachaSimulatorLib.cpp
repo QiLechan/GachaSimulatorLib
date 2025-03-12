9125e4b9dc43c05a664c85132f5be9fcef48ac57
admin <admin@uaq7sdgjra0hct9>
2025-03-12 15:50:09 +0800

    添加项目文件。

---
+++ GachaSimulatorLib/GachaSimulatorLib.cpp
@@ -0,0 +1,19 @@
+﻿// GachaSimulatorLib.cpp : 定义静态库的函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <cjson/cJSON.h>
+
+// 创建初始JSON配置
+char* createGenshinJson()
+{
+	cJSON* root = cJSON_CreateObject();
+
+	cJSON_AddItemToObject(root, "version", cJSON_CreateString("1.0.0"));
+	cJSON_AddItemToObject(root, "description", "cJSON_CreateString("原神抽卡配置"));
+}
+
+
+
+
